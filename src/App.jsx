1cb6ce763c794187f91e5afd9b02a76de3d0b292
Harsh0-0Singh <harshsingh9hrs@gmail.com>
2025-11-10 17:27:55 +0530

    Project completion

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import gsap from "gsap";
 import {ScrollTrigger,SplitText} from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Menu from "./components/Menu";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Contact from "./components/Contact";
 gsap.registerPlugin(ScrollTrigger,SplitText);
 const App = () => {
   return (
@@ -14,6 +16,9 @@ const App = () => {
         <Cocktails/>
         <About/>
         <Art/>
+        <Menu/>
+        <Contact/>
+
     </main>
   )
 }
